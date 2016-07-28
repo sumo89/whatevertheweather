c7d08ce0e46df8883a90df6c6a08d5beb39e2e30
Henry Summers <ssumo89@gmail.com>
2016-07-28 14:13:55 +0100

    working on toggle 2

--- main.js
+++ main.js
@@ -15,27 +15,50 @@ var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
 var apiKey = '0230bf02702536cff53832eef68ab815';
 
 var userLocation = {};
+var tempReplace = {};
 
 app.getData = function(){
 	$.ajax({
 		url: weatherUrl + userLocation.place + "&appid=" + apiKey,
 		success: function(response){
-			var returnWeather = response.weather[0].main;
-			console.log(returnWeather);
+			var weatherDescriptionMain = response.weather[0].main;
+			var weatherDescriptionDetail = response.weather[0].description;
+			var temperature = Math.floor(response.main.temp - 273.15);
+			var humidity = response.main.humidity;
+			var windSpeed = response.wind.speed;
+			var myDate = new Date(response.sys.sunset *1000);
+
+			$('#weatherDescriptionMain').html(weatherDescriptionMain);
+			$('#weatherDescriptionDetail').html(weatherDescriptionDetail);
+			$('#temperature').html(temperature + "°C");
+			$('#humidity').html(humidity, + "\%");
+			$('#windSpeed').html(windSpeed + "m/s");
+			$('#listTemp').html(myDate.toLocaleString());
 		}
 	});
+};
+
+app.tempToggle = function(){
+	$(this).toggleClass('toggle-button-selected');
+	$('#farenheit').toggleClass('farenheit');
+	$('#celcius').toggle();
+	tempReplace.temp = $('#temperature').val();
+
+	$('#temperature').html((tempReplace.temp * 9/5 + 32), "°F");
+	console.log(tempReplace.temp);
 };
 
 app.handleSubmit = function(event){
 	event.preventDefault();
 	userLocation.place = $('#location').val();
 	$('#location').focus().val('');
-	$('#userLocation').html(userLocation);
+	$('#userLocation').html(userLocation.place);
 	app.getData();
 };
 
 app.init = function(){
 	$('form').on('submit', app.handleSubmit);
+	$('#toggle-button').on('click', app.tempToggle);
 };
 
 $(document).ready(app.init);
